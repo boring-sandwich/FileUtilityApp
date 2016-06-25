003b15be645088a78dc7aa92454969fa5fcd7a7c
boring-sandwich <github@geekypanda.com>
2016-06-25 16:23:44 -0700

    Version One of File Copy and Delete App

---
+++ UtilityApps/CopyOnePasteToMany.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+using System.IO;
+
+namespace UtilityApps
+{
+    /// <summary>
+    /// Interaction logic for CopyOnePasteToMany.xaml
+    /// </summary>
+    public partial class CopyOnePasteToMany : Window
+    {
+        private FolderBrowserDialog fldBD = new FolderBrowserDialog();
+        private OpenFileDialog file = new OpenFileDialog();
+
+
+        int whatStep = 1;
+        string sourcePath;
+        string targetPath;
+        string excludePath;
+
+        bool excludeFolderMode;
+        bool deleteFileMode;
+
+        public CopyOnePasteToMany(bool excludeMode, bool deleteMode)
+        {
+            InitializeComponent();
+            excludeFolderMode = excludeMode;
+            deleteFileMode = deleteMode;
+            WhatStep();
+            FileWindow.Title = deleteFileMode ? "Delete File From Folders" : "Copy File To Foldlers";
+        }
+        private void WhatStep()
+        {
+            switch (whatStep)
+            {
+                case 1:
+                    txbSteps.Text = $"Step {whatStep} of " + (excludeFolderMode ? "4." : "3.");
+                    txbInstructions.Text = $"What file do you want to " + (deleteFileMode ? "delete?" : "copy?");
+                    HiddenObjects();
+                    btnNext.Content = "next";
+                    break;
+                case 2:
+                    txbSteps.Text = $"Step {whatStep} of " + (excludeFolderMode ? "4." : "3.");
+                    txbInstructions.Text = $"Where is the main folder?";
+                    HiddenObjects();
+                    if (!deleteFileMode)
+                        cbxUserSelection.Visibility = Visibility.Visible;
+                    break;
+                case 3:
+                    txbSteps.Text = $"Step {whatStep} of " + (excludeFolderMode ? "4." : "3.");
+                    txbInstructions.Text = $"What folder do you want to exclude?";
+                    txbInstructions.Visibility = Visibility.Visible;
+                    HiddenObjects();
+                    break;
+                case 4:
+                    txbSteps.Text = $"Complete.";
+                    HiddenObjects();
+                    btnNext.Visibility = Visibility.Visible;
+                    btnNext.Content = "close";
+                    btnBrowse.Visibility = Visibility.Hidden;
+                    CopyFile();
+                    break;
+            }
+        }
+
+        private void CopyFile()
+        {
+            string[] directories = Directory.GetDirectories(targetPath, "*", SearchOption.TopDirectoryOnly);
+            foreach (string directory in directories)
+            {
+                if (directory != excludePath)
+                {
+                    string destinationPath = Path.Combine(directory, Path.GetFileName(sourcePath));
+                    if (cbxUserSelection.IsChecked.Value == true)
+                    {
+                           File.Copy(sourcePath, destinationPath, true);
+                    }
+                    else
+                    {
+                        bool sourceExists = File.Exists(destinationPath);
+                        if (deleteFileMode && sourceExists)
+                            File.Delete(destinationPath);
+
+                        if (!sourceExists)
+                        {
+                            if (deleteFileMode)
+                                continue;
+                            else
+                                File.Copy(sourcePath, destinationPath);
+                        }
+                    }
+                }                  
+            }
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            if (whatStep == 1)
+            {
+                file.ShowDialog();
+                sourcePath = file.FileName;
+                txbInput.Text = sourcePath;
+                VisibleObjects();
+            }
+            else if (whatStep == 2)
+            {
+                if (fldBD.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    targetPath = fldBD.SelectedPath;
+                    txbInput.Text = targetPath;
+                    VisibleObjects();
+                }
+            }
+            else if (whatStep == 3)
+            {
+                if (fldBD.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    excludePath = fldBD.SelectedPath;
+                    txbInput.Text = excludePath;
+                    VisibleObjects();
+                }
+            }
+        }
+
+        private void VisibleObjects()
+        {
+            if (whatStep != 1)
+            {
+                txbInstructions.Visibility = Visibility.Hidden;
+            }
+            txbInput.Visibility = Visibility.Visible;
+            btnNext.Visibility = Visibility.Visible;
+        }
+        private void HiddenObjects()
+        {
+            txbInput.Visibility = Visibility.Hidden;
+            btnNext.Visibility = Visibility.Hidden;
+            cbxUserSelection.Visibility = Visibility.Hidden;
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            if (whatStep == 1)
+            {
+                whatStep = 2;
+            }
+            else if(whatStep == 2)
+            {
+                if (excludeFolderMode)
+                {
+                    whatStep = 3;
+                }
+                else
+                {
+                    whatStep = 4;
+                }
+            }
+            else if (whatStep == 3)
+            {
+                whatStep = 4;
+            }
+            else if (whatStep == 4)
+            {
+                this.Close();
+                return;
+            }
+            WhatStep();
+        }
+    }
+}
---
+++ UtilityApps/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+
+namespace UtilityApps
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool excludeMode = true;
+        bool deleteMode = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNewDialogue_Click(object sender, RoutedEventArgs e)
+        {
+            LaunchDialogue(sender);
+        }
+
+        private void LaunchDialogue(object sender)
+        {
+            if (sender == btnCopyFileAndPasteToMany || sender == btnCopyFileAndPasteToMany_ButNotThisOne)
+            {
+                deleteMode = false;
+                excludeMode = !(sender == btnCopyFileAndPasteToMany);
+            }
+            else if (sender == btnDeleteFileFromMany || sender == btnDeleteFileFromMany_ButNotThisOne)
+            {
+                deleteMode = true;
+                excludeMode = !(sender == btnDeleteFileFromMany);
+            }
+            try
+            {
+                CopyOnePasteToMany windowFile = new CopyOnePasteToMany(excludeMode, deleteMode);
+                windowFile.ShowDialog();
+            }
+            catch
+            {
+                ErrorFailedToOpenWindow();
+            }
+        }
+        private static void ErrorFailedToOpenWindow()
+        {
+            MessageBox.Show("Failed to load window. Please restart the application.", "Advice", MessageBoxButton.OK);
+        }
+    }
+}
